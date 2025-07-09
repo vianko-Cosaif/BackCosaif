5f2dda6b212028f8111cc3ad112aaf7ffbd33d2c
Isaac Serrano Campos <odin99campos@gmail.com>
2025-07-09 17:18:26 -0600

    Ignore dist/ y node_modules

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// index.ts
-require("./config/firebase"); // 1??  Primera l�nea: inicializa
-const Servidor_1 = require("./Servidor/Servidor");
-(0, Servidor_1.iniciarServidor)();
