3bdd7bb75b498cd9ce0e48f30bf8f7ec0888d99e
Isaac Serrano Campos <isaac.serrano@vianko.com.mx>
2025-07-25 12:49:19 -0600

    CAMBIO BUENO

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config"); // carga .env
 require("./config/firebase"); // inicializa Firebase (si lo usas)
-require("./cron/cleanupTokens"); // ¡agenda el job de limpieza!
+require("./Cron/cleanupTokens"); // ¡agenda el job de limpieza!
 const Servidor_1 = require("./Servidor/Servidor");
 (0, Servidor_1.iniciarServidor)();
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 
 import 'dotenv/config';               // carga .env
 import './config/firebase';           // inicializa Firebase (si lo usas)
-import './cron/cleanupTokens';        // ¡agenda el job de limpieza!
+import './Cron/cleanupTokens';        // ¡agenda el job de limpieza!
 import { iniciarServidor } from './Servidor/Servidor';
 
 iniciarServidor();
