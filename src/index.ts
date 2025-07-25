be2c100922e4b62e13565d3a4f957f5fa13a0e3a
Isaac Serrano Campos <isaac.serrano@vianko.com.mx>
2025-07-25 12:52:44 -0600

    CAMBIO BUENO1

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
 
 import 'dotenv/config';               // carga .env
 import './config/firebase';           // inicializa Firebase (si lo usas)
-import './Cron/cleanupTokens';        // ¡agenda el job de limpieza!
+import './Cron/Tokens';        // ¡agenda el job de limpieza!
 import { iniciarServidor } from './Servidor/Servidor';
 
 iniciarServidor();
