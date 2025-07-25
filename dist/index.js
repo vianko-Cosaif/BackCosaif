743aaa3dca195a9bcefbc590c757e061b962cf37
Isaacvko123 <isaac.serrano@vianko.com.mx>
2025-07-25 18:57:29 +0000

    Ya porfa

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config"); // carga .env
 require("./config/firebase"); // inicializa Firebase (si lo usas)
-require("./Cron/cleanupTokens"); // ¡agenda el job de limpieza!
+require("./Cron/Tokens"); // ¡agenda el job de limpieza!
 const Servidor_1 = require("./Servidor/Servidor");
 (0, Servidor_1.iniciarServidor)();
